ec9127d59f8916a9948c0a8828f71bcd812d6944
sridharu346 <sridharu346@gmail.com>
2023-06-12 11:13:25 -0400

    first commit

---
+++ dbconfig/connection.go
@@ -0,0 +1,16 @@
+package dbconfig
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func DBConnection() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:Sri@7272@tcp(127.0.0.1:3306)/housedetails")
+	if err != nil {
+		fmt.Println("Error in connectiong the DB", err)
+		return nil, err
+	}
+	fmt.Println("Database is connected successfully")
+	return db, nil
+}
